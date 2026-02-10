617336f80252bf188dcc3aaf62a81e12bc0d5965
Srijak1202 <srijak1202@gmail.com>
2026-02-10 10:05:46 +0530

    the utils updated to read the .env file

--- backend/etl/utils.py
+++ backend/etl/utils.py
@@ -1,15 +1,20 @@
 import hashlib
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
 
 # -----------------------------
 # Database Connection
 # -----------------------------
 def get_db_connection():
     return psycopg2.connect(
-        host="localhost",
-        database="study_analytics",
-        user="postgres",
-        password="jayesh@2005"
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
 
 # -----------------------------
